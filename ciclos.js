f62f6b1e71d0d38604c5fb6b8bab128ca6484575
GustavoCreo <gustavocreo@yahoo.com.ar>
2022-05-07 16:27:26 -0300

    orden del codigo

--- ciclos.js
+++
@@ -1,7 +0,0 @@
-let entrada = prompt("¿Que va a pedir?");
-
-while(entrada != "Nada más" ){
-    alert("Se agregó al pedido "+ entrada);
-    
-    entrada = prompt("¿Desea algo más?");
-}
---
+++ js/ciclos.js
@@ -0,0 +1,18 @@
+
+//let entrada = prompt("¿Que va a pedir?");
+
+//while(entrada != "Nada más" ){
+    //alert("Se agregó al pedido "+ entrada);
+    
+    //entrada = prompt("¿Desea algo más?");
+//}
+
+let nombreUsuario = prompt("Ingresar nombre de usuario");
+
+if (nombreUsuario == "") {
+    alert("No ingresaste el nombre de usuario");
+}
+else {
+    alert("Bienvenido/a " + nombreUsuario);
+}
+
