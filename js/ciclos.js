30329e6fb64b6702897456b9ce01ee7a5760e7f0
GustavoCreo <gustavocreo@yahoo.com.ar>
2022-05-07 17:59:15 -0300

    Agregando function

--- js/ciclos.js
+++ js/ciclos.js
@@ -1,18 +1,32 @@
 
-//let entrada = prompt("¿Que va a pedir?");
+/*let entrada = prompt("¿Qué quiere pedir?");
 
-//while(entrada != "Nada más" ){
-    //alert("Se agregó al pedido "+ entrada);
+while(entrada != "Nada más" ){
+    alert("Se agregó al pedido "+ entrada);
     
-    //entrada = prompt("¿Desea algo más?");
-//}
+    entrada = prompt("¿Desea algo más?");
+}*/
 
+//prompt de bienvenida 
 let nombreUsuario = prompt("Ingresar nombre de usuario");
+//function que tome los datos ingresados
+function nombrecompleto(nombre, apellido) {
+    nombreUsuario = nombre + apellido
+}
+//Alert que da la bienvenida
+alert("Bienvenido " + nombreUsuario)
+//No estoy seguro si lo arme bien, la idea es que tome los datos del prompt para hacer el alert mediante el function, espero a la corrección
+
+
+
+
+
+/*let nombreUsuario = prompt("Ingresar nombre de usuario");
 
 if (nombreUsuario == "") {
     alert("No ingresaste el nombre de usuario");
 }
 else {
     alert("Bienvenido/a " + nombreUsuario);
-}
+}*/
 
