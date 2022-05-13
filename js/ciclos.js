7af9c7b4167c3befa1f09eac40dc1cd1c15f9952
GustavoCreo <gustavocreo@yahoo.com.ar>
2022-05-13 14:14:26 -0300

    function

--- js/ciclos.js
+++ js/ciclos.js
@@ -1,32 +1,7 @@
 
-/*let entrada = prompt("¿Qué quiere pedir?");
+function solicitarNombre(){
+    let nombreIngresado   = prompt("Ingresar nombre")
+    alert("Benvenido/a " + nombreIngresado)
+} 
 
-while(entrada != "Nada más" ){
-    alert("Se agregó al pedido "+ entrada);
-    
-    entrada = prompt("¿Desea algo más?");
-}*/
-
-//prompt de bienvenida 
-let nombreUsuario = prompt("Ingresar nombre de usuario");
-//function que tome los datos ingresados
-function nombrecompleto(nombre, apellido) {
-    nombreUsuario = nombre + apellido
-}
-//Alert que da la bienvenida
-alert("Bienvenido " + nombreUsuario)
-//No estoy seguro si lo arme bien, la idea es que tome los datos del prompt para hacer el alert mediante el function, espero a la corrección
-
-
-
-
-
-/*let nombreUsuario = prompt("Ingresar nombre de usuario");
-
-if (nombreUsuario == "") {
-    alert("No ingresaste el nombre de usuario");
-}
-else {
-    alert("Bienvenido/a " + nombreUsuario);
-}*/
-
+solicitarNombre("")
